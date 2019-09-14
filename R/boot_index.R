3381e5afea59adc68f16d130d1a6866b870d02d3
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2019-09-14 02:41:48 -0700

    first pass at redesign of boot_index

--- R/boot_index.R
+++ R/boot_index.R
@@ -1,22 +1,44 @@
 #' Bootsrapped randomly samples values
 #'
-#' @param data a data.frame
+#' @param data a data.frame with data summaries by a variable.
+#' @param data_full a data.frame which is the unsummaried version of data.
 #' @param id variable name, to be aggregated around
 #' @param value variable name, contains the value to take mean over
 #' @param reps Number of reps, defaults to 1000
 #'
 #' @return a data.frame
+#' @importFrom rlang :=
 #' @export
-boot_index <- function(data, id, value, reps = 1000) {
-  ddd <- data %>%
-    ii_summarize({{id}}, {{value}})
+bootter <- function(data, data_full,
+                    value, id, reps) {
 
-  sample_mean <- function(n, value, data, replicates = reps) {
-    mean(value < replicate(replicates, mean(sample(data, n))))
-  }
+  lengths <- sort(unique(data$n))
 
-  ddd$pvalue <- purrr::map2_dbl(ddd$length, ddd$value,
-                         ~ sample_mean(.x, .y, dplyr::pull(sample_data, {{value}})))
+  big_data <- data_full %>%
+    dplyr::filter(!is.na({{id}}))
 
-  ddd
+  varname <- paste(rlang::as_name(rlang::ensym(value)), "_v1")
+  data %>%
+    dplyr::mutate(
+      !!varname := perm_v1(pwd = dplyr::pull(data, {{value}}),
+                           n = data$n,
+                           full_sites = dplyr::pull(data_full, {{value}}),
+                           n_rep = reps,
+                           lengths = lengths)
+    )
 }
+
+perm_v1_inner <- function(n_sites, n_rep, data) {
+  replicate(
+    n = n_rep,
+    expr = mean(sample(data, as.numeric(n_sites), TRUE))
+  )
+}
+
+#' @importFrom purrr map2_dbl
+perm_v1 <- function(pwd, n, full_sites, n_rep, lengths) {
+  names(lengths) <- lengths
+  res <- lapply(lengths, perm_v1_inner, n_rep = n_rep, data = full_sites)
+  map2_dbl(pwd, n, ~ mean(.x < res[[as.character(.y)]]))
+}
+
--- R/ii_summarize.R
+++ R/ii_summarize.R
@@ -3,13 +3,19 @@
 #' @param data a data.frame
 #' @param id variable name, to be aggregated around
 #' @param value variable name, contains the value to take mean over
+#' @param fun function, summary statistic function  to be calculated. Defaults
+#' to `mean`.
 #'
+#' @importFrom rlang .data
 #' @return data.frame
 #' @export
-ii_summarize <- function(data, id, value) {
-  data %>%
+ii_summarize <- function(data, id, value, fun = mean) {
+  grouped_data <- data %>%
     dplyr::filter(!is.na({{id}})) %>%
-    dplyr::group_by({{id}}) %>%
-    dplyr::summarise(value = mean({{value}}),
-                     length = dplyr::n())
+    dplyr::group_by({{id}})
+
+  dplyr::bind_cols(
+    dplyr::summarise_at(grouped_data, dplyr::vars({{value}}), fun),
+    dplyr::count(grouped_data) %>% dplyr::ungroup() %>% dplyr::select(.data$n)
+  )
 }
